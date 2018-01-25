6ac488c6f227b4054647464624551792934755f2
GuttermanA <alexanderf.gutterman@gmail.com>
2018-01-25 14:34:40 -0500

    added menu class

---
+++ lib/app/menu.rb
@@ -0,0 +1,22 @@
+class Menu
+
+  def self.login
+    create_user_or_login
+  end
+
+  def self.main
+
+  end
+
+  def self.stats
+
+  end
+
+  def self.search
+
+  end
+
+  def self.yelp_reviews
+
+  end
+end
