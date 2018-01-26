7d5d914d9297df3626f9ca1c8653056bda75eda9
morgannegagne <morgannegagne@gmail.com>
2018-01-26 09:38:25 -0500

    new api key

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ ActiveRecord::Base.logger = nil
 API_HOST = "https://api.yelp.com"
 SEARCH_PATH = "/v3/businesses/search"
 BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
-
+API_KEY = ENV["YELP_API"]
 
 DEFAULT_BUSINESS_ID = "yelp-san-francisco"
 DEFAULT_TERM = "dinner"
